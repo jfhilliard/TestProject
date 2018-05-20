11422eb90311ca65ecd7042ace03797e091a93e9
Jonathan Hilliard <jfhilliard@gmail.com>
2018-05-20 15:07:16 -0700

    Update unittest for pep8

--- Sandbox/test/test_dct.py
+++ Sandbox/test/test_dct.py
@@ -4,6 +4,7 @@ import unittest
 import numpy as np
 import scipy.fftpack
 
+from numpy.testing import assert_allclose
 from Sandbox.utils.dct import dct, idct, dct2
 
 
@@ -29,7 +30,7 @@ class TestDCT(unittest.TestCase):
         """Test DCT with a variety of inputs"""
         for x, expected in zip(self.x_list, self.expected_list):
             y = dct(x)
-            np.testing.assert_allclose(y, expected, atol=1e-10)
+            assert_allclose(y, expected, atol=1e-10)
 
     def test_vs_scipy_dct(self):
         """Test that my DCT gives same result as scipy"""
@@ -38,7 +39,7 @@ class TestDCT(unittest.TestCase):
             y_scipy = scipy.fftpack.dct(x)
 
             # Scipy has a scale factor of 2 difference
-            np.testing.assert_allclose(y, y_scipy / 2.0, atol=1e-10)
+            assert_allclose(y, y_scipy / 2.0, atol=1e-10)
 
     def test_inverse_dct(self):
         """Test that the dct->idct process gets you back to the input."""
@@ -46,7 +47,7 @@ class TestDCT(unittest.TestCase):
             y = dct(x)
             x_inv = idct(y)
 
-            np.testing.assert_allclose(x_inv, x, atol=1e-10)
+            assert_allclose(x_inv, x, atol=1e-10)
 
 
 class TEST2DDCT(unittest.TestCase):
@@ -70,30 +71,36 @@ class TEST2DDCT(unittest.TestCase):
             return x
 
         # Create some test data with the same freqency in each dimension
-        x_cos1 = [np.cos((np.arange(N) + 0.5) * x * np.pi / N) for x in range(N)]
+        x_cos1 = [np.cos((np.arange(N) + 0.5) * x * np.pi / N)
+                  for x in range(N)]
+
         self.x_cos2_list = [np.outer(x_cos, x_cos) for x_cos in x_cos1]
         self.expected_cos2_list = [create_expected(n, n) for n in range(N)]
 
         # Create some test data with different frequencies in each dimension
-        y_cos1 = [np.cos((np.arange(N) + 0.5) * x * np.pi / N) for x in range(N-1, -1, -1)]
-        self.x_cos2_list += [np.outer(x_cos, y_cos) for x_cos, y_cos in zip(x_cos1, y_cos1)]
-        self.expected_cos2_list += [create_expected(n, m) for n, m in enumerate(range(N-1, -1, -1))]
+        y_cos1 = [np.cos((np.arange(N) + 0.5) * x * np.pi / N)
+                  for x in range(N-1, -1, -1)]
+
+        self.x_cos2_list += [np.outer(x_cos, y_cos)
+                             for x_cos, y_cos in zip(x_cos1, y_cos1)]
+
+        self.expected_cos2_list += [create_expected(n, m)
+                                    for n, m in enumerate(range(N-1, -1, -1))]
 
     def test_dct2(self):
         for n, x in enumerate(self.x_cos2_list):
             y = dct2(x)
-            np.testing.assert_allclose(y, self.expected_cos2_list[n], atol=1e-10)
-
-        self.x_cos2_list = [np.outer(x_cos, x_cos) for x_cos in x_cos1]
+            assert_allclose(y, self.expected_cos2_list[n], atol=1e-10)
 
     def test_vs_scipy_dct2(self):
-        """Test that my DCT gives same result as scipy"""
+        """Test that my DCT2 gives same result as scipy"""
         for x in self.x_cos2_list:
-            y = dct(x)
+            y = dct2(x)
             y_scipy = scipy.fftpack.dctn(x)
 
-            # Scipy has a scale factor of 2 difference
-            np.testing.assert_allclose(y, y_scipy / 2.0, atol=1e-10)
+            # Scipy has a scale factor of 2^2 difference
+            assert_allclose(y, y_scipy / (2.0**2.0), atol=1e-10)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
